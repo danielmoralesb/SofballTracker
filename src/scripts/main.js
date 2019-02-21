3c4905f829fae6050deb67afab9d18f0d45ac9db
danielmoralesb <daniel_morales81@hotmail.com>
2019-02-21 13:01:07 -0500

    Master Files Upload

---
+++ src/scripts/main.js
@@ -0,0 +1,24 @@
+// function addToTeamModal() {
+//     const modalWrapper = document.querySelector('.modal-wrapper');
+//     const modal = document.querySelector('.modal');
+//     const players = document.getElementById('players')
+//     const player = players.querySelectorAll('.item');
+//     const close = document.querySelector('.modal__close');
+//     const closeBtn = document.querySelector('.modal__footer .--light');
+
+//     function openModal() {
+//         modalWrapper.classList.remove('--hide');
+//     }
+
+//     function closeModal() {
+//         modalWrapper.classList.add('--hide');
+//     }
+
+//     player.forEach(player => player.addEventListener('click', openModal));
+//     close.addEventListener('click', closeModal);
+//     closeBtn.addEventListener('click', closeModal);
+
+//     //modalWrapper.addEventListener('click', closeModal);
+// }
+
+// setTimeout(addToTeamModal, 3000);
